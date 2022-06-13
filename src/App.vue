5b5ac5c9a8688491116fcbfbc2252e71b799efaa
bocman <bocmancul@bk.ru>
2022-06-13 21:59:49 +0400

    one

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 <script setup>
 import { RouterView } from 'vue-router'
-console.log(123321111);
+console.log(12332111111);
 console.log('dev');
 </script>
 <style lang="scss" src="./style/main.scss"></style>
