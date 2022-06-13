94e81512a446a1d451c566713f65593f2235ef17
bocman <bocmancul@bk.ru>
2022-06-13 22:38:12 +0400

    new three!!!

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 <script setup>
 import { RouterView } from 'vue-router'
-console.log(12332111111333);
+console.log(12332111111);
 console.log('dev');
 </script>
 <style lang="scss" src="./style/main.scss"></style>
