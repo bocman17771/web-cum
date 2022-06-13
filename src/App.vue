d7fee2a9147b34e8e821f2e1c4b2a03abb40c94c
bocman <bocmancul@bk.ru>
2022-06-13 21:57:43 +0400

    chore merge

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,7 @@
 </template>
 <script setup>
 import { RouterView } from 'vue-router'
-<<<<<<< HEAD
 console.log(123321111);
-=======
 console.log('dev');
->>>>>>> 0973e8256ebfda934d058b8e711636eafbc723e7
 </script>
 <style lang="scss" src="./style/main.scss"></style>
