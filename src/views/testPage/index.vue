7891b4eb982f76239053eaedc4deff3e442282ad
bocman <bocmancul@bk.ru>
2022-06-13 13:47:26 +0400

    chore test

--- src/views/testPage/index.vue
+++ src/views/testPage/index.vue
@@ -1,8 +1,57 @@
 <template>
-  test
+<!--  <div id="map" style="width: 600px; height: 400px"></div>-->
+  <loading v-if="loading">loading...</loading>
+  <template v-else>
+    <yandex-map :settings="settings">
+<!--      <ymap-marker-->
+<!--          :coords="coords"-->
+<!--          marker-id="123123"-->
+<!--          marker-type="placemark"-->
+<!--      />-->
+    </yandex-map>
+  </template>
 </template>
-<script lang="ts"> export default { name: 'testPage' } </script>
+<script lang="ts">
+import { yandexMap, ymapMarker } from 'vue-yandex-maps';
+import testComponent from './testComponent.vue';
+
+console.log(yandexMap)
+export default {
+  name: 'testPage',
+  components: {
+    yandexMap,
+    ymapMarker,
+    testComponent,
+  }
+}
+</script>
 <script lang="ts" setup>
+import {ref} from "vue";
+
+const loading = ref(true)
+setTimeout(() => {
+  loading.value = false
+}, 2000)
+
+const coords = [54.82896654088406, 39.831893822753904]
+const settings = {
+  apiKey: '5c81cb20-2376-4a88-a4d8-7016fd97df75',
+  lang: 'ru_RU',
+  coordorder: 'latlong',
+  enterprise: false,
+  version: '2.1'
+}
+
+
+// ymaps.ready(init)
+// console.log(ymaps)
+// function init(){
+//   var myMap = new ymaps.Map("map", {
+//     center: [55.76, 37.64],
+//     zoom: 7
+//   });
+// }
+
 // const arr = ref<any[]>([false,1,0,1,2,0,1,3,"a"])
 
 // arr.value.sort((a,b):void => {
